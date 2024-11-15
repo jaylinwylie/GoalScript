2615b03e4511e32eefe3ad7f8c35d4fcd2b17c26
jaylinwylie <jaylinwylie@gmail.com>
2024-11-15 13:47:32 -0800

    Add mermaid rendering for goal objects

--- main.py
+++ main.py
@@ -1,39 +1,11 @@
-from goal import Goal, GoalArray
+from goal import GoalArray
+from mermaid_renderer import list_of_goals_to_mermaid
 from parser import GolScriptParser
 
-GOLSCRIPT = 'CleanFountain.golscript'
+GOLSCRIPT = 'Today.golscript'
 
 with open(GOLSCRIPT) as file:
     golscript = file.read()
-
-
-def goal_to_mermaid(goal: Goal, generated: set):
-    mermaid_str = []
-    if goal.name in generated:
-        return mermaid_str
-    generated.add(goal.name)
-
-    for non_ref in goal.non_reference:
-        mermaid_str.append(f"{non_ref} --> {goal.name}")
-
-    for ref in goal.reference:
-        mermaid_str.append(f"{ref.name} --> {goal.name}")
-        mermaid_str.extend(goal_to_mermaid(ref, generated))
-
-    for opt in goal.optional:
-        mermaid_str.append(f"{opt.name} -.- {goal.name}")
-        mermaid_str.extend(goal_to_mermaid(opt, generated))
-
-    return mermaid_str
-
-
-def list_of_goals_to_mermaid(_goals):
-    generated = set()
-    mermaid_diagram = ['graph TD']
-    for goal in _goals:
-        mermaid_diagram.extend(goal_to_mermaid(goal, generated))
-    return "\n".join(mermaid_diagram)
-
 
 goals: GoalArray = GolScriptParser().parse_script(golscript)
 
---
+++ mermaid_renderer.py
@@ -0,0 +1,44 @@
+from goal import Goal
+
+
+def goal_to_mermaid(goal: Goal, generated: set):
+    mermaid_str = []
+    if goal.name in generated:
+        return mermaid_str
+    generated.add(goal.name)
+
+    if len(goal.tasks) > 0:
+        task_name = goal.name + 'Tasks'
+        task_str = "\n".join([task for task in goal.tasks])
+        if goal.is_all:
+            mermaid_str.append(f'{task_name}[\\{task_str}/] ==> {goal.name}')
+        else:
+            mermaid_str.append(f'{task_name}{{{task_str}}} ==> {goal.name}')
+
+    else:
+        task_name = goal.name
+        if goal.is_all:
+            mermaid_str.append(f'{task_name}[\\{goal.name}/]')
+        else:
+            mermaid_str.append(f'{task_name}[/{goal.name}\\]')
+
+    for non_ref in goal.non_reference:
+        mermaid_str.append(f"{non_ref}(({non_ref})) ---> {task_name}")
+
+    for ref in goal.reference:
+        mermaid_str.append(f"{ref.name} --> {task_name}")
+        mermaid_str.extend(goal_to_mermaid(ref, generated))
+
+    for opt in goal.optional:
+        mermaid_str.append(f"{opt.name} -.-> {task_name}")
+        mermaid_str.extend(goal_to_mermaid(opt, generated))
+
+    return mermaid_str
+
+
+def list_of_goals_to_mermaid(_goals):
+    generated = set()
+    mermaid_diagram = ['graph TD']
+    for goal in _goals:
+        mermaid_diagram.extend(goal_to_mermaid(goal, generated))
+    return "\n".join(mermaid_diagram)
