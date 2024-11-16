b26e2cbcb3608138ef30c943fc345fdab83ea291
jaylin <jaylinwylie@gmail.com>
2024-11-15 22:02:19 -0800

    Add is_completed attribute

--- goal.py
+++ goal.py
@@ -1,12 +1,12 @@
 class Goal:
     def __init__(self):
         self.is_all: bool | None = None
+        self.is_completed: bool = False
         self.name: str | None = None
         self.tasks: list = []
         self.non_reference: list[str] = []
         self.reference: list[Goal] = []
         self.optional: list[Goal] = []
-        self.is_completed: bool = False
 
 
 class GoalArray:
